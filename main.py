ff3580efbac6e65dcac55576aa4d45679a80a7a8
Alessandro Diogo Bruckheimer <alebruck@gmail.com>
2012-03-05 23:56:49 -0300

    add world, glass, fog

--- main.py
+++ main.py
@@ -17,25 +17,29 @@ import direct.directbase.DirectStart
 from direct.gui.DirectGui import * 
 from direct.task import Task 
 
-#import world, characters
+import world
+#, characters
 
 class Main(DirectObject):
 	side = True
-	score = 0
 
 	def __init__(self):
 		print "game init"
 		# start Game
 		self.config()
 		taskMgr.add(self.timerTask, 'timerTask')
+		self.setCamera()
+		a = world.World()
 
 	# define some game configuration
 	def config(self):
 		# scape to exit
 		self.accept("escape",sys.exit)
 		# define game clock label
-		self.mytimer = DirectLabel(scale=.05,pos=(0,0,0))
-		self.score = DirectLabel(scale=.05,pos=(0,5,0))
+		self.mytimer = DirectLabel(scale=.05,pos=(-0.95,0,0.9))
+		self.score = DirectLabel(scale=.05,pos=(0.95,0,0.9))
+
+		self.score['text'] = str(0)
 
 		# 30 seconds
 		taskMgr.doMethodLater(30, self.changeSide, 'changeSide')
@@ -51,12 +55,16 @@ class Main(DirectObject):
 		self.secondsTime = int(task.time) 
 	  	self.minutesTime = int(self.secondsTime/60)
 	  	self.mytimer['text'] = "%02d:%02d" % (self.minutesTime%60, self.secondsTime%60)
-	  	print self.side
 	  	return Task.cont
 
 	# change current game side
 	def changeSide(self, task):
 		self.side = not self.side
+
+	# define camera position
+	def setCamera(self):
+		base.cam.setHpr(0,-15,0)
+		base.cam.setPos(0, 0, 200)
 
 
 
--- world.py
+++ world.py
@@ -11,11 +11,77 @@ from direct.actor.Actor import Actor
 from direct.showbase.DirectObject import DirectObject
 import random, sys, os, math
 
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import *
+from direct.interval.IntervalGlobal import *
+import sys
+
+from pandac.PandaModules import *
+#for directx object support
+from direct.showbase.DirectObject import DirectObject
+#for tasks
+from direct.task import Task
+#for Actors
+from direct.actor.Actor import Actor
+#for Pandai
+from panda3d.ai import *
+
+import random
+import socket
+from pandac.PandaModules import TransparencyAttrib 
+
+import sys, random
+import direct.directbase.DirectStart
+from pandac.PandaModules import *
+from direct.interval.IntervalGlobal import *
+from direct.gui.DirectGui import *
+
+from direct.filter.CommonFilters import CommonFilters
+
 class World():
 
 	def __init__(self):
 		print "world started"
+		self.createGround()
+		self.createWalls()
+		self.startFog()
 
-	# create de game environ
-	def createWord(self):
-		print "world created"
+	def createGround(self):
+		cm = CardMaker("ground")
+		cm.setFrame(0, 1000, 0, 1000)
+		ground = render.attachNewNode(cm.generate())
+		ground.setP(270)
+		ground.setPos(-500,300,0)
+		tex = loader.loadTexture("misc/images/solo.jpg")
+		ground.setTexture(tex)
+
+	def createWalls(self):
+
+		tex = loader.loadTexture("misc/images/water.png")
+
+		glass1 = CardMaker("plane1")
+		glass1.setFrame(0, 1000, 0, 1000)
+		glass1 = render.attachNewNode(glass1.generate())
+		glass1.setHpr(90,180,0)
+		glass1.setTransparency(TransparencyAttrib.MAlpha)
+		glass1.setPos(200,300,500)
+		glass1.setTexture(tex)
+
+		glass2 = CardMaker("plane2")
+		glass2.setFrame(0, 1000, 0, 1000)
+		glass2 = render.attachNewNode(glass2.generate())
+		glass2.setHpr(270,180,0)
+		glass2.setTransparency(TransparencyAttrib.MAlpha)
+		glass2.setPos(-200,1300,500)
+		glass2.setTexture(tex)
+
+	def startFog(self):
+		colour = (0.2,0.6,0.6)
+		linfog = Fog("A linear-mode Fog node")
+		linfog.setColor(*colour)
+		linfog.setLinearRange(500,1000)
+		render.attachNewNode(linfog)
+		render.setFog(linfog)
+		base.setBackgroundColor( colour )
+
+
